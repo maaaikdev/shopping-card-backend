f90020961e3b1a2c2883b99cb687c0cd845ab4a6
Michael Reyes <maaaik.dev@gmail.com>
2024-05-06 03:46:35 -0500

    My first commit

---
+++ products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image: "https://maaaik.site/images/iphone12pro.jpg" 
+    },
+    {
+        id: 2,
+        name: "iPhone 12",
+        desc: "5.4-inch display",
+        price: 699,
+        image: "https://maaaik.site/images/iphone12.jpg" 
+    },
+    {
+        id: 3,
+        name: "Galaxy S",
+        desc: "6.5-inch display",
+        price: 399,
+        image: "https://maaaik.site/images/galaxyS.png" 
+    }
+];
+
+module.exports = products
