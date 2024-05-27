c38d82ab24cbe90fe702d4bfda9bef871259176c
Michael Reyes <maaaik.dev@gmail.com>
2024-05-27 10:03:23 -0500

    add some products

--- products.js
+++ products.js
@@ -3,23 +3,34 @@ const products = [
         id: 1,
         name: "iPhone 12 Pro",
         desc: "6.1-inch display",
-        price: 999,
+        price: 15000,
+        off: 12000,
         image: "https://maaaik.site/images/iphone12pro.jpg" 
     },
     {
         id: 2,
         name: "iPhone 12",
         desc: "5.4-inch display",
-        price: 699,
+        price: 20000,
+        off: 17000,
+        image: "https://maaaik.site/images/iphone12.jpg" 
+    },    
+    {
+        id: 3,
+        name: "iPhone 12",
+        desc: "5.4-inch display",
+        price: 25000,
+        off: 21000,
         image: "https://maaaik.site/images/iphone12.jpg" 
     },
     {
-        id: 3,
+        id: 4,
         name: "Galaxy S",
         desc: "6.5-inch display",
-        price: 399,
+        price: 24000,
+        off: 18000,
         image: "https://maaaik.site/images/galaxyS.png" 
-    }
+    },
 ];
 
 module.exports = products
